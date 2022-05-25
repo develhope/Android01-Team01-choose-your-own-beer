2c74bc9bc49fa0e83061c95b57fec36dc3ce56c3
Matteo <matteocatalano@protonmail.com>
2022-05-25 17:49:02 +0200

    Added punk service Added internet permission Implement retrofit Gson and lifecycle Clean main activity and build retrofit client Create method to consider the possibility of network failure

--- app/src/main/java/co/develhope/chooseyourownbeer/MainActivity.kt
+++ app/src/main/java/co/develhope/chooseyourownbeer/MainActivity.kt
@@ -1,15 +1,35 @@
 package co.develhope.chooseyourownbeer
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.develhope.chooseyourownbeer.databinding.ActivityMainBinding
+import co.develhope.chooseyourownbeer.network.BeersResult
+import co.develhope.chooseyourownbeer.network.PunkService
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    //Build retrofit
+    var retrofit = Retrofit.Builder()
+        .baseUrl("https://api.punkapi.com/v2/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    //Create class
+    var punkService = retrofit.create(PunkService::class.java)
 
     private lateinit var binding: ActivityMainBinding
 
@@ -19,6 +39,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Call method for beer repo
+        retrievePunkRepos()
+
+        //Hide actionBar
         supportActionBar?.hide()
 
         val navView: BottomNavigationView = binding.navView
@@ -34,4 +58,18 @@ class MainActivity : AppCompatActivity() {
         setupWithNavController(navView, navController)
         navView.setupWithNavController(navController)
     }
+    fun retrievePunkRepos(){
+        lifecycleScope.launch{
+            try{
+                val repos = punkService.listRepos("beers")
+                //@TODO showRepos(repos)
+            } catch (e: Exception){
+                Log.e("MainActivity", "Error retrieving repos: $e")
+                Snackbar.make(findViewById(R.id.container), "Error retrieving repos",
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Retry"){ retrievePunkRepos() }.show()
+            }
+        }
+    }
+    //@TODO create function showRepos(repos)
 }
---
+++ app/src/main/java/co/develhope/chooseyourownbeer/network/BeersResult.kt
@@ -0,0 +1,5 @@
+package co.develhope.chooseyourownbeer.network
+
+import co.develhope.chooseyourownbeer.model.Beer
+
+class BeersResult : ArrayList<Beer>()
