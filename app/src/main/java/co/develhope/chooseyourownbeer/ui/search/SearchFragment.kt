1d68ee5b66eaba2f6aead7c4c85e2e6da3cc812a
Giusy Mancuso <giusy219@gmail.com>
2022-05-25 17:27:13 +0200

    Move the logic of the listener into smaller functions

--- app/src/main/java/co/develhope/chooseyourownbeer/ui/search/SearchFragment.kt
+++ app/src/main/java/co/develhope/chooseyourownbeer/ui/search/SearchFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.SearchView
-import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -23,50 +21,65 @@ class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var listFiltered: List<Beer>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener,
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
             androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextChange(query: String?): Boolean {
                 if (query != null) {
-                    var listFiltered = Beers.getFilteredBeer(query)
-                    var count = listFiltered.size
-                    binding.textResult.text ="$count Risultati"
-                    binding.beerListSearch.apply {
-                        adapter = BeerAdapter(listFiltered) { action -> onAdapterClick(action) }
-                        layoutManager= LinearLayoutManager(context)
-                    }
-                } else {
-                    Toast.makeText(context, "Nessun elemento trovato", Toast.LENGTH_LONG).show()
+                    listFiltered = Beers.getFilteredBeer(query)
+                    val count = listFiltered.size
+                    printCount(count)
+                    showListFiltered()
                 }
-                return false
+                return true
             }
+
             override fun onQueryTextSubmit(query: String?): Boolean {
-                /*               if (beerList.contains(query)) {
-                                   adapterQuery?.filter?.filter(query)
-                               } else {
-                                   Toast.makeText(context, "Nessun elemento trovato", Toast.LENGTH_LONG).show()
-                               }
-               */                return false
+                if (query != null) {
+                    listFiltered = Beers.getFilteredBeer(query)
+                    val count = listFiltered.size
+                    printCount(count)
+                    showListFiltered()
+                }
+                return true
             }
         })
     }
-    private fun onAdapterClick(action:BeerAction){
+
+    private fun printCount(count: Int) {
+        if (count != 0) {
+            binding.textResult.text = "$count Risultati"
+        } else {
+            binding.textResult.text = "Nessun elemento trovato"
+        }
+    }
+
+    private fun showListFiltered() {
+        binding.beerListSearch.apply {
+            adapter = BeerAdapter(listFiltered) { action -> onAdapterClick(action) }
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun onAdapterClick(action: BeerAction) {
         when (action) {
             //        is BeerAction.OnStarClick
             is BeerAction.OnGoToDetailPageClick -> {
-                val idBeer= action.beer.id
-                val bundle= bundleOf("BEER_ID" to idBeer)
+                val idBeer = action.beer.id
+                val bundle = bundleOf("BEER_ID" to idBeer)
                 findNavController().navigate(R.id.action_navigation_search_to_beerDetail, bundle)
             }
         }
