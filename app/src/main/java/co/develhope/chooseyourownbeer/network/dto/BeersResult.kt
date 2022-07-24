fa0bb0922e39c7d45f40acc1f6a255ebde38d7b6
Mich9419 <mike.work9419@gmail.com>
2022-07-25 01:05:59 +0200

    ModifyBeerResult

--- app/src/main/java/co/develhope/chooseyourownbeer/network/dto/BeersResult.kt
+++ app/src/main/java/co/develhope/chooseyourownbeer/network/dto/BeersResult.kt
@@ -1,3 +1,32 @@
 package co.develhope.chooseyourownbeer.network.dto
 
-class BeersResult : ArrayList<BeerDTO>()
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+private lateinit var beers : BeersResult
+
+class BeersResult
+
+private var _beersResult = MutableLiveData<BeerDTO>()
+private val beersResult : LiveData<BeerDTO>
+    get() = _beersResult
+private var _error = MutableLiveData<BeerDTO>()
+private val error : LiveData<BeerDTO>
+    get() = _error
+
+fun retrieveRepos() {
+    CoroutineScope(Dispatchers.Main).launch {
+        try {
+            _beersResult.value = null
+            val beers = beersResult
+            val error = error
+            _error.value = (_error.value)
+        } catch (e : Exception) {
+
+        }
+    }
+}
